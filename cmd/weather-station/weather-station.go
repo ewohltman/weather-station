69184ebd1b722794d2bad7bf841e7043f83ac067
Eric Wohltman <eric.wohltman@gmail.com>
2022-12-16 10:48:05 -0500

    Add HTML and Javascript

--- cmd/weather-station/weather-station.go
+++ cmd/weather-station/weather-station.go
@@ -1,12 +1,17 @@
+//go:build js && wasm
+// +build js,wasm
+
 // Package main is the entry point to the program.
 package main
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"github.com/ewohltman/weather-station/internal/weather"
 	"log"
 	"net/http"
-
-	"github.com/ewohltman/weather-station/internal/weather"
+	"syscall/js"
 )
 
 const (
@@ -14,21 +19,44 @@ const (
 	long = "-74.064810"
 )
 
+func registerCallbacks() {
+	js.Global().
+		Get("document").
+		Call("getElementById", "addButton").
+		Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) any {
+			go func() {
+				ctx := context.Background()
+				httpClient := &http.Client{}
+
+				apiClient, err := weather.NewAPIClient(ctx, httpClient, lat, long)
+				if err != nil {
+					log.Fatalf("Error creating new API client: %s", err)
+				}
+
+				forecast, err := apiClient.QueryForecast(ctx)
+				if err != nil {
+					log.Fatalf("Error querying forecast: %s", err)
+				}
+
+				buffer := &bytes.Buffer{}
+
+				for _, period := range forecast.Properties.Periods {
+					_, _ = buffer.WriteString(fmt.Sprintf("%s: Temp: %d, %s\n", period.Name, period.Temperature, period.ShortForecast))
+				}
+
+				js.Global().Get("document").Call("getElementById", "output").Set("innerHTML", buffer.String())
+				fmt.Println("button clicked")
+
+			}()
+
+			return nil
+		}))
+}
+
 func main() {
-	ctx := context.Background()
-	httpClient := &http.Client{}
+	c := make(chan struct{}, 0)
 
-	apiClient, err := weather.NewAPIClient(ctx, httpClient, lat, long)
-	if err != nil {
-		log.Fatalf("Error creating new API client: %s", err)
-	}
+	registerCallbacks()
 
-	forecast, err := apiClient.QueryForecast(ctx)
-	if err != nil {
-		log.Fatalf("Error querying forecast: %s", err)
-	}
-
-	for _, period := range forecast.Properties.Periods {
-		log.Printf("%s: Temp: %d, %s\n", period.Name, period.Temperature, period.ShortForecast)
-	}
+	<-c
 }
